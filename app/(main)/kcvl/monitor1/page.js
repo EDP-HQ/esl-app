774d779a10ddb8ddfe58bd813a216cb4115918b9
Sheik Hazrin <sheikhazrin@kiswire.com>
2024-02-07 14:20:36 +0800

    monitor 2,3 & 4

--- app/(main)/kcvl/monitor1/page.js
+++ app/(main)/kcvl/monitor1/page.js
@@ -3,11 +3,15 @@
 
 "use client";
 
-import React, { useEffect, useState } from 'react';
+import React, { useState, useRef, useEffect } from "react";
+import { InputMask } from "primereact/inputmask";
+import { Button } from 'primereact/button';
+import { Toast } from 'primereact/toast';
+import { TabView, TabPanel } from "primereact/tabview";
+
+
 // Core viewer
 import { Worker, Viewer } from '@react-pdf-viewer/core';
-
-// Plugins
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 
 // Import styles
@@ -16,105 +20,131 @@ import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
 // Create new plugin instance
 
+import {qcfiles_search } from "../../../api/kcvl";
 
 const Monitor1 = () => {
     const defaultLayoutPluginInstance = defaultLayoutPlugin();
     const workerUrl = "/pdf.worker.js";
 
-    const pdfFiles = [
-        "Cord Type Check Result Jan 2024.pdf",
-        "Testing result of LLE in Jan. 2024.pdf",
-        // "Daily report of FD tool (2401).pdf",
-    ];
+    const toast = useRef(null);
+    const dta = useRef(null);
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [YYMM, setYYMM] = useState(null);
+    const [YYMMDD, setYYMMDD] = useState(null);
+    const [loading, setLoading] = useState(false)
+    const [resultDT, setResultDT] = useState([])
+    const [fGroup, setFGroup] = useState('CT'); // CT, TWBN - , FD, LLE
 
-    
+
+    const handleKeyPress = (e) => {
+        // console.log('handleKeyPress', e);
+        if (e.key === 'Enter') {
+            actionSearch()
+        }
+    }
+    const handleLoad = () => {
+        const currentDate = new Date();
+        const currentYear = currentDate.getFullYear();
+        const currentMonth = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+        const currentDay = currentDate.getDate().toString().padStart(2, '0');
+
+        // const adjustMonth = '11'
+        const formattedYYMM = `${currentYear}${currentMonth}`;
+        const formattedYYMMDD = `${currentYear}${currentMonth}${currentDay}`;
+        setYYMM(formattedYYMM)
+        setYYMMDD(formattedYYMMDD)
+
+        // loading 1st data
+        handleFetchData(formattedYYMM)
+    }
+
+    const handleFetchData = async (todate) => {
+        setLoading(true)
+        try {
+            if (todate && todate.length === 6) {
+                //  console.log ('1- handleFetchData' , todate)
+                const _paramsDT = {
+                    "ToDate": todate,
+                    "fileGroup": fGroup,
+                    "CancelYN": 'N'
+                }
+                const _result = await qcfiles_search(_paramsDT)
+                console.log('_result', _result.data)
+
+                if (_result.status === 200) {
+                    setResultDT(_result.data)
+
+                }
+            } else {
+                // setResultDT([])
+            }
+        } catch (error) {
+            console.error('handleFetchData ', error);
+            toast.current.show({ severity: 'error', summary: 'Warning', detail: { error }, life: 3000 });
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    const actionSearch = () => {
+        let myDate = YYMM.replace('/', '');
+        setYYMM(myDate)
+        handleFetchData(myDate)
+    }
+
 
     useEffect(() => {
-        // Dynamically set the worker URL based on the current location
-        // const currentLocation = window.location.origin;
-        // console.log('current location', currentLocation)
+        handleLoad()
     }, []);
 
     return (
         <div className="grid">
             <div className="col-12">
+                <Toast ref={toast} />
                 <div className="card">
-                    <h5>Monitor #1</h5>
-                    Loading PDF Files
-                    
-                    {/* <Worker workerUrl={workerUrl}>
-                        <div
-                            style={{
-                                display: 'flex',
-                                justifyContent: 'space-between',
-                                marginTop: '20px', // Adjust the margin as needed
-                            }}
-                        >
-                            {pdfFiles.map((pdfFile, index) => (
-                                <div
-                                    key={index}
-                                    style={{
-                                        height: '750px',
-                                        width: '30%', // Adjust the width as needed
-                                        marginLeft: 'auto',
-                                        marginRight: 'auto',
-                                    }}
-                                >
-                                    <Viewer fileUrl={`/upload/${pdfFile}`} plugins={[defaultLayoutPluginInstance]} />
-                                </div>
-                            ))}
-                        </div>
-                    </Worker> */}
+                    <h5>Monitor : TW Cord type Check Result{'   '}
+                        <InputMask value={YYMM} onChange={(e) => { setYYMM(e.target.value); }} mask="9999/99" placeholder="Year/Month" onKeyUp={handleKeyPress} />
+                        <Button label="Search" icon="pi pi-search" outlined onClick={actionSearch} />
+                    </h5>
+                    <TabView
+                        activeIndex={activeIndex}
+                        onTabChange={(e) => setActiveIndex(e.index)}
+                    >
+                        <TabPanel header="Preview" >
+                            {resultDT.length > 0 ? (
+                                resultDT.map((resultData, index) => (
+                                    <Worker workerUrl={workerUrl} key={index}>
+                                        <div
+                                            style={{
+                                                height: '750px',
+                                                marginLeft: 'auto',
+                                                marginRight: 'auto',
+                                            }}
+                                        >
+                                            <Viewer fileUrl={`/upload/${resultData.fileUploadName}`} plugins={[defaultLayoutPluginInstance]} />
+                                        </div>
+                                    </Worker>
+                                ))
+                            ) : (
+                                <div>-- No Data Found. --</div>
+                            )}
+                        </TabPanel>
+                        <TabPanel header="List" >
+                            <ol>
+                                {resultDT.length > 0 ? (
+                                    resultDT.map((resultData, index) => (
+                                        <li key={index}>
+                                            {resultData.fileName} {'    '}
+                                            <a href={'/upload/' + resultData.fileUploadName} target='_blank' title='View Files'><i className="pi pi-file-pdf"></i></a>
+                                        </li>
+                                    ))
+                                ) : (
+                                    <div>-- No Data Found. --</div>
+                                )}
+                            </ol>
+                        </TabPanel>
 
-
-                  
-
-                    {pdfFiles.map((pdfFile, index) => (
-                        <Worker workerUrl={workerUrl} key={index}>
-                            <div
-                                key={index}
-                                style={{
-                                    height: '750px',
-                                    // width: '30%', // Adjust the width as needed
-                                    marginLeft: 'auto',
-                                    marginRight: 'auto',
-                                }}
-                            >
-                                <Viewer key={index} fileUrl={`/upload/${pdfFile}`} plugins={[defaultLayoutPluginInstance]} />
-                            </div>
-                        </Worker>
-                    ))}
-
-
-                                        
-                    {/* // <Worker id={1} workerUrl={workerUrl}>
-                    //     <div
-                    //         style={{
-                    //             height: '750px',
-                    //             marginLeft: 'auto',
-                    //             marginRight: 'auto',
-                    //         }}
-                    //     >
-                    //         <Viewer fileUrl={`/upload/${pdfFiles[1]}`} plugins={[defaultLayoutPluginInstance]} />
-
-                    //     </div>
-                    // </Worker>
-
-                    // <Worker id={2} workerUrl={workerUrl}>
-                    //     <div
-                    //         style={{
-                    //             height: '750px',
-                    //             marginLeft: 'auto',
-                    //             marginRight: 'auto',
-                    //         }}
-                    //     >
-                    //         <Viewer fileUrl={`/upload/${pdfFiles[2]}`} plugins={[defaultLayoutPluginInstance]} />
-
-                    //     </div>
-                    // </Worker> */}
-
-
-
+                    </TabView>
 
                 </div>
             </div>
--- app/(main)/kcvl/monitor2/page.js
+++ app/(main)/kcvl/monitor2/page.js
@@ -3,11 +3,15 @@
 
 "use client";
 
-import React, { useEffect, useState } from 'react';
-// Core viewer
-import { Worker,Viewer } from '@react-pdf-viewer/core';
+import React, { useState, useRef, useEffect } from "react";
+import { InputMask } from "primereact/inputmask";
+import { Button } from 'primereact/button';
+import { Toast } from 'primereact/toast';
+import { TabView, TabPanel } from "primereact/tabview";
 
-// Plugins
+
+// Core viewer
+import { Worker, Viewer } from '@react-pdf-viewer/core';
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 
 // Import styles
@@ -16,37 +20,131 @@ import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
 // Create new plugin instance
 
+import {qcfiles_search } from "../../../api/kcvl";
 
-const Monitor1 = () => {
-    const [workerUrl, setWorkerUrl] = useState('');
-
+const Monitor2 = () => {
     const defaultLayoutPluginInstance = defaultLayoutPlugin();
+    const workerUrl = "/pdf.worker.js";
+
+    const toast = useRef(null);
+    const dta = useRef(null);
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [YYMM, setYYMM] = useState(null);
+    const [YYMMDD, setYYMMDD] = useState(null);
+    const [loading, setLoading] = useState(false)
+    const [resultDT, setResultDT] = useState([])
+    const [fGroup, setFGroup] = useState('TWBN'); // CT, TWBN - , FD, LLE
+
+
+    const handleKeyPress = (e) => {
+        // console.log('handleKeyPress', e);
+        if (e.key === 'Enter') {
+            actionSearch()
+        }
+    }
+    const handleLoad = () => {
+        const currentDate = new Date();
+        const currentYear = currentDate.getFullYear();
+        const currentMonth = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+        const currentDay = currentDate.getDate().toString().padStart(2, '0');
+
+        // const adjustMonth = '11'
+        const formattedYYMM = `${currentYear}${currentMonth}`;
+        const formattedYYMMDD = `${currentYear}${currentMonth}${currentDay}`;
+        setYYMM(formattedYYMM)
+        setYYMMDD(formattedYYMMDD)
+
+        // loading 1st data
+        handleFetchData(formattedYYMM)
+    }
+
+    const handleFetchData = async (todate) => {
+        setLoading(true)
+        try {
+            if (todate && todate.length === 6) {
+                //  console.log ('1- handleFetchData' , todate)
+                const _paramsDT = {
+                    "ToDate": todate,
+                    "fileGroup": fGroup,
+                    "CancelYN": 'N'
+                }
+                const _result = await qcfiles_search(_paramsDT)
+                console.log('_result', _result.data)
+
+                if (_result.status === 200) {
+                    setResultDT(_result.data)
+
+                }
+            } else {
+                // setResultDT([])
+            }
+        } catch (error) {
+            console.error('handleFetchData ', error);
+            toast.current.show({ severity: 'error', summary: 'Warning', detail: { error }, life: 3000 });
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    const actionSearch = () => {
+        let myDate = YYMM.replace('/', '');
+        setYYMM(myDate)
+        handleFetchData(myDate)
+    }
+
 
     useEffect(() => {
-        // Dynamically set the worker URL based on the current location
-        const currentLocation = window.location.origin;
-        console.log('current location', currentLocation)
+        handleLoad()
     }, []);
 
     return (
         <div className="grid">
             <div className="col-12">
+                <Toast ref={toast} />
                 <div className="card">
-                    <h5>Monitor #2</h5>
-                    Loading PDF Files
-                   
-                    <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.16.105/build/pdf.worker.js">
-                        <div
-                            style={{
-                                height: '750px',
-                                marginLeft: 'auto',
-                                marginRight: 'auto',
-                            }}
-                        >
-                            <Viewer fileUrl="/upload/Daily report of FD tool (2401).pdf" plugins={[defaultLayoutPluginInstance]} />
-                        </div>
-                    </Worker>
+                    <h5>Monitor : TWBN-WRCP Tool{'   '}
+                        <InputMask value={YYMM} onChange={(e) => { setYYMM(e.target.value); }} mask="9999/99" placeholder="Year/Month" onKeyUp={handleKeyPress} />
+                        <Button label="Search" icon="pi pi-search" outlined onClick={actionSearch} />
+                    </h5>
+                    <TabView
+                        activeIndex={activeIndex}
+                        onTabChange={(e) => setActiveIndex(e.index)}
+                    >
+                        <TabPanel header="Preview" >
+                            {resultDT.length > 0 ? (
+                                resultDT.map((resultData, index) => (
+                                    <Worker workerUrl={workerUrl} key={index}>
+                                        <div
+                                            style={{
+                                                height: '750px',
+                                                marginLeft: 'auto',
+                                                marginRight: 'auto',
+                                            }}
+                                        >
+                                            <Viewer fileUrl={`/upload/${resultData.fileUploadName}`} plugins={[defaultLayoutPluginInstance]} />
+                                        </div>
+                                    </Worker>
+                                ))
+                            ) : (
+                                <div>-- No Data Found. --</div>
+                            )}
+                        </TabPanel>
+                        <TabPanel header="List" >
+                            <ol>
+                                {resultDT.length > 0 ? (
+                                    resultDT.map((resultData, index) => (
+                                        <li key={index}>
+                                            {resultData.fileName} {'    '}
+                                            <a href={'/upload/' + resultData.fileUploadName} target='_blank' title='View Files'><i className="pi pi-file-pdf"></i></a>
+                                        </li>
+                                    ))
+                                ) : (
+                                    <div>-- No Data Found. --</div>
+                                )}
+                            </ol>
+                        </TabPanel>
 
+                    </TabView>
 
                 </div>
             </div>
@@ -54,4 +152,4 @@ const Monitor1 = () => {
     );
 };
 
-export default Monitor1;
+export default Monitor2;
